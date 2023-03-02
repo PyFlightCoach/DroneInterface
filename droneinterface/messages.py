548eef0c01ac5e4872dbf5310d948b104e4cb197
thomas david <thomasdavid0@gmail.com>
2023-03-02 11:20:50 +0000

    message dialects and id lookup

--- droneinterface/messages.py
+++ droneinterface/messages.py
@@ -1,19 +1,36 @@
-from pymavlink.dialects.v20.all import *
-from flightanalysis.base.collection import Collection
 
+from pymavlink.dialects.v20.all import MAVLink_message as v2Message
+from pymavlink.dialects.v10.all import MAVLink_message as v1Message
+
+from flightanalysis.base.collection import Collection
+from typing import Union
+from numbers import Number
 
 
 class Messages(Collection):
     VType = type
     uid = "msgname"
     
+    def __init__(self, data):
+        super().__init__(data)
+        self.idmap = {m.id: m for m in self}
     
-    def id_lookup(self, id):
-        for v in self:
-            if v.id == id:
-                return v
+    @staticmethod
+    def parse_dialect(dialect="v2"):
+        MM = v1Message if dialect == "v1" else v2Message
+        return Messages([Mess for Mess in MM.__subclasses__() if hasattr(Mess, "msgname")])
 
-messages = Messages([Mess for Mess in MAVLink_message.__subclasses__() if hasattr(Mess, "msgname")])
+    def from_id(self, id: Union[int, list]):
+        if isinstance(id, int):
+            return self.idmap[id]
+        elif isinstance(id, list):
+            return Messages([self.idmap[i] for i in id])
+        else:
+            raise AttributeError("expected an int or list")
 
-    
-pass
+    def subset(self, *messages):
+        return Messages([getattr(self, m) for m in messages])
+
+if __name__ == "__main__":
+    messages = Messages.parse_dialect()
+    pass    
